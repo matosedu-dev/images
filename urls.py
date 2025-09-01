0513273387a185d94f3a00428ddc82ec6f88d797
matosedu-dev <matosedu@outlook.com>
2025-09-01 11:31:36 -0300

    py_005

--- urls.py
+++ urls.py
@@ -1,7 +1,12 @@
 urls = [
 
-"https://down-br.img.susercontent.com/file/sg-11134201-7rdyf-md0egl9vd93ib5.webp",
-"https://down-br.img.susercontent.com/file/sg-11134201-7rdyf-md0egl9vd93ib5.webp"
+"https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/be17288dd0a949e4a0cd4c1443c6daf8_9366/minibola--copa-do-mundo-de-clubes-fifa-25.jpg",
+"https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/76a98214c20c48b486a457da140e8f13_9366/minibola-ucl-25-26-league-stage.jpg",
+"https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/c633cf68c0a444e69071880c53c65ea0_9366/bola-ucl-league-25-26-league-stage-boxed.jpg",
+"https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/3ec090234f264021b84bf3f24f9565d1_9366/bola-treino-ucl-25-26-league-stage.jpg",
+"https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/75ca25328aed42fc8f67ae3d012a7b76_9366/bola-basquete-tres-listras-rubber-x3.jpg",
+"https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/d7c85a3c1a6845d1a375274ad2f9f33f_9366/bola-ucl-pro-sala-24-25-eliminatorias.jpg",
+"https://assets.adidas.com/images/w_383,h_383,f_auto,q_auto,fl_lossy,c_fill,g_auto/96414a576a15414bbc7e684d33c0556d_9366/bola-universadi.jpg"
 
 ]
 
